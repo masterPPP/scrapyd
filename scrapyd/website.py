a2c522001ab423e9c318cbcb87870c2fe03af58b
phk52 <1204819479@qq.com>
2015-09-08 22:53:20 +0800

    添加自定义的spiders页面

--- scrapyd/website.py
+++ scrapyd/website.py
@@ -17,12 +17,18 @@ class Root(resource.Resource):
         resource.Resource.__init__(self)
         self.debug = config.getboolean('debug', False)
         self.runner = config.get('runner')
+        datadir = config.get('data_dir')
         logsdir = config.get('logs_dir')
         itemsdir = config.get('items_dir')
         local_items = itemsdir and (urlparse(itemsdir).scheme.lower() in ['', 'file'])
         self.app = app
         self.nodename = config.get('node_name', socket.gethostname())
         self.putChild('', Home(self, local_items))
+
+        self.putChild('spiders', Spiders(self, local_items))
+
+        if datadir:
+            self.putChild('data', static.File(datadir, 'text/plain'))
         if logsdir:
             self.putChild('logs', static.File(logsdir, 'text/plain'))
         if local_items:
@@ -74,6 +80,7 @@ class Home(resource.Resource):
 <h1>Scrapyd</h1>
 <p>Available projects: <b>%(projects)s</b></p>
 <ul>
+<li><a href="/spiders">Spiders</a></li>
 <li><a href="/jobs">Jobs</a></li>
 """ % vars
         if self.local_items:
@@ -149,3 +156,108 @@ class Jobs(resource.Resource):
         s += "</body>"
         s += "</html>"
         return s
+
+from .utils import get_spider_list
+from .config import Config
+from apscheduler.schedulers.twisted import TwistedScheduler
+import logging
+import os
+
+class Spiders(resource.Resource):
+    u"""
+    显示Spider的工作状态以及调度计划
+    """
+    config = Config()
+    def __init__(self, root, local_items):
+        resource.Resource.__init__(self)
+        self.root = root
+        self.local_items = local_items
+        self.spider_status_dic = {}
+
+        logging.basicConfig()
+        self.scheduler = TwistedScheduler()
+        self.scheduler.start()
+
+    def get_spider_status(self, project):
+        spider_status = self.spider_status_dic.get(project)
+        if not spider_status:
+            spider_status = dict((spider, {'status': 'finished', 'timestamp': None, 'job': None, 'schedule_job': None})
+                                 for spider in get_spider_list(project))
+            self.spider_status_dic[project] = spider_status
+        self._update_spider_status(project)
+        return spider_status
+
+    def _update_spider_status(self, project):
+        u"""
+        先获取目前任务调度情况，然后再获取apsheduler中的任务。
+        """
+        spider_status = self.spider_status_dic.get(project)
+        for project, queue in self.root.poller.queues.items():
+            for m in queue.list():
+                spider = m['name']
+                job = m['_job']
+                spider_status[spider]['status'] = 'pending'
+                spider_status[spider]['timestamp'] = None
+                spider_status[spider]['job'] = job
+        for p in self.root.launcher.processes.values():
+            spider = p.spider
+            spider_status[spider]['status'] = 'running'
+            spider_status[spider]['timestamp'] = p.start_time
+            spider_status[spider]['job'] = p.job
+        for p in self.root.launcher.finished:
+            spider = p.spider
+            spider_status[spider]['status'] = 'finished'
+            spider_status[spider]['timestamp'] = p.end_time
+            spider_status[spider]['job'] = p.job
+
+        for spider in spider_status:
+            status = spider_status[spider]
+            sjob = self.scheduler.get_job(spider)
+            status['schedule_job'] = sjob
+            if sjob:
+                status['next_time'] = sjob.next_run_time
+            else:
+                status['next_time'] = None
+            # sjob._get_run_times()
+
+    def render_GET(self, txrequest):
+        args = dict((k, v[0]) for k, v in txrequest.args.items())
+        project = args.pop('project', 'careerTalk')
+        spider_status = self.get_spider_status(project)
+        content = "<tr>"
+        for th in ['spider', 'status', 'timestamp', 'next_time', 'data']:
+            content += "<th>%s</th>" % th
+        content += "</tr>"
+        for spider in spider_status:
+            status = spider_status[spider]
+            content += "<tr>"
+            content += "<td>%s</td><td>%s</td><td>%s</td><td>%s</td>" \
+                       % (spider, status['status'], status['timestamp'], status['next_time'])
+            content += "<td><a href='/data/%s/'>data</a></td>" % spider
+            content += "</tr>"
+        sub_form = "<form action='' method='post'><input type='submit'>start all</input></form>"
+        html = "<table>"+content+"</table>"+sub_form
+        return html
+
+    def render_POST(self, txrequest):
+        args = dict((k, v[0]) for k, v in txrequest.args.items())
+        project = args.pop('project', 'careerTalk')
+        spiders = ['NJU', 'BIT', 'ECUST', 'RUC']
+
+        for spider in spiders:
+            job = self.scheduler.add_job(spider_crawl, 'interval', minutes=3, id=spider, args=[project, spider])
+            job.executor
+        # self.scheduler.add_job(test_job, 'interval', seconds=1)
+
+
+def spider_crawl(project, spider):
+    # print project, spider
+    http_port = Spiders.config.getint('http_port', 6800)
+    bind_address = Spiders.config.get('bind_address', '0.0.0.0')
+
+    url = 'http://%s:%s/schedule.json' % (bind_address, http_port)
+    cmd = "curl %s -d project=%s -d spider=%s" % (url, project, spider)
+    logging.info('start apscheduler_job: '+cmd)
+    os.system(cmd)
+
+    return "<span>任务全部开启</span><a href='/'>返回</a>"
